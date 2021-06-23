d85a08abc6a74a94fa6df4faf28a21f01003f64f
Monali-W <wasekarmonaligmail.com>
2021-06-23 17:24:24 -0400

    Presentational and container component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React,{ Component } from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
+
 import "./App.css";
-import Directory from "./components/DirectoryComponent";
+import Main from "./components/MainComponent";
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">NuCamp</NavbarBrand>
-          </div>
-        </Navbar>
-        <Directory />
-      </div>
+        <Main/>
+     </div>
     );
   }
 }
